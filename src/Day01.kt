0a90e3d827a91d83f2cf175e5d18fe59ef98dff6
Guillaume Quittet <guillaume.quittet@gmail.com>
2025-12-07 12:00:21 +0100

    feat(day1): add part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val start = 50
+    val circleSize = 100
+
+    fun dialToMoves(dial: String): Int {
+        val steps = dial.drop(1).toInt()
+        val direction = when (dial.first()) {
+            'L' -> -1
+            'R' -> 1
+            else -> error("Invalid direction: ${dial.first()}")
+        }
+        return steps * direction
     }
+
+    fun part1(input: List<String>): Int =
+        input.scan(start) { total, dial -> (total + dialToMoves(dial)).mod(circleSize) }.count { it == 0 }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
